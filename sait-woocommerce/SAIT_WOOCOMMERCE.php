91a2c482e146c8242d7fb6ab7e91dcd678c2a3a0
ali <ali@saitenlinea.com>
2024-03-25 14:51:02 -0700

    agregar replyto a correos

--- sait-woocommerce/SAIT_WOOCOMMERCE.php
+++ sait-woocommerce/SAIT_WOOCOMMERCE.php
@@ -92,3 +92,24 @@ function sendOrderSAIT_thankyou( $order_id ){
 	SAIT_WOOCOMMERCE_Orders::SAIT_sendOrderThankyou($order_id );
 }
 
+
+// Filtro para incluir reply to a correo.
+
+function wp_mail_smtp_dev_reply_to( $args ) {
+  $reply_to = 'Reply-To: Ventas <'.get_bloginfo('admin_email').'>';
+  if ( ! empty( $args['headers'] ) ) {
+  if ( ! is_array( $args['headers'] ) ) {
+   $args['headers'] = array_filter( explode( "\n", str_replace( "\r\n", "\n", $args['headers'] ) ) );
+  }
+   // Filter out all other Reply-To headers.
+  $args['headers'] = array_filter( $args['headers'], function ( $header ) {
+   return strpos( strtolower( $header ), 'reply-to' ) !== 0;
+  } );
+ } else {
+  $args['headers'] = [];
+ }
+  $args['headers'][] = $reply_to;
+  return $args;
+}
+ 
+add_filter( 'wp_mail', 'wp_mail_smtp_dev_reply_to', PHP_INT_MAX );
