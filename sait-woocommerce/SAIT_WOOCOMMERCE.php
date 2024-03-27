8ca6c81cc2b328f1bc68944898495fc76775d7b8
ali <ali@saitenlinea.com>
2024-03-27 14:53:39 -0700

    revertido reply to

--- sait-woocommerce/SAIT_WOOCOMMERCE.php
+++ sait-woocommerce/SAIT_WOOCOMMERCE.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package SAIT_WOOCOMMERCE
- * @version 1.0.22
+ * @version 1.0.23
  */
 /*
 Plugin Name: SAIT WooCommerce
 Description: Este plugin agrega un endpoint a wordpress para procesar eventos enviados desde SAIT.
 Author: SAIT Software Administrativo
-Version: 1.0.22
+Version: 1.0.23
 Author URI: http://sait.mx
 */
 
@@ -93,23 +93,3 @@ function sendOrderSAIT_thankyou( $order_id ){
 }
 
 
-// Filtro para incluir reply to a correo.
-
-function wp_mail_smtp_dev_reply_to( $args ) {
-  $reply_to = 'Reply-To: Ventas <'.get_bloginfo('admin_email').'>';
-  if ( ! empty( $args['headers'] ) ) {
-  if ( ! is_array( $args['headers'] ) ) {
-   $args['headers'] = array_filter( explode( "\n", str_replace( "\r\n", "\n", $args['headers'] ) ) );
-  }
-   // Filter out all other Reply-To headers.
-  $args['headers'] = array_filter( $args['headers'], function ( $header ) {
-   return strpos( strtolower( $header ), 'reply-to' ) !== 0;
-  } );
- } else {
-  $args['headers'] = [];
- }
-  $args['headers'][] = $reply_to;
-  return $args;
-}
- 
-add_filter( 'wp_mail', 'wp_mail_smtp_dev_reply_to', PHP_INT_MAX );
