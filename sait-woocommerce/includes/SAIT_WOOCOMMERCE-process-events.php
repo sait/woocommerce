a3c33079c486e79e3c7044462fbe4d4f3dadc8eb
ali <ali@saitenlinea.com>
2024-11-19 17:42:06 -0700

    process-events: usar responses y request de SAIT_UTILS.

--- sait-woocommerce/includes/SAIT_WOOCOMMERCE-process-events.php
+++ sait-woocommerce/includes/SAIT_WOOCOMMERCE-process-events.php
@@ -54,35 +54,33 @@
 				$res = self::MODCLI($oXml);
 				break;
 			default:
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("OK");
+				$res = SAIT_UTILS::SAIT_response(200,"OK");
 				break;
 		}
 		return $res;
 	}
 
+	//
+	// dividir esta func, add,upd,delete.
+	//
+
 	public static function MODART($oXml){
 		// Proceso de MODART
-
+		$oKeys = $oXml->action[0]->keys[0];
+		$oFlds = $oXml->action[0]->flds[0];
 		// Saco la clave del articulo
-		$clave = self::getClaves("arts",trim(self::xml_attribute($oXml->action[0]->keys[0],"numart")),null);
+		$clave = SAIT_UTILS::SAIT_getClaves("arts",trim(self::xml_attribute($oKeys,"numart")),null);
 
 		// Si viene con statusweb=0 salir
-		$statusweb = self::xml_attribute($oXml->action[0]->flds[0],"statusweb");
+		$statusweb = self::xml_attribute($oFlds,"statusweb");
 		if ($statusweb == "0") {
 			// Si existe lo manda a la papelera
 			if (isset($clave->wcid)){
 				wp_trash_post($clave->wcid);
 			}
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("OK");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"OK");
 		}
 		
-		// Toma los campos del articulo 
-		$productflds = $oXml->action[0]->flds[0];
 
 		// Si ya existe el articulo en la tabla de SAIT solo actualizar datos
 		if (isset($clave->wcid)) {
@@ -91,46 +89,40 @@
 			if ($product===false) {
 				// Producto ya no existe borrarlo de tabla SAIT
 				// para evitar conflictos
-				self::deleteClaves($clave->id);
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("ART NO EXISTE");
-				return $res;
+				SAIT_UTILS::SAIT_deleteClaves($clave->id);
+				return SAIT_UTILS::SAIT_response(200,"ART NO EXISTE");
 			}
 			// wp_untrash_post si el id esta en la papelera sacarlo
 			wp_untrash_post($clave->wcid);
 
 			// Actualizar producto
 			$product = wc_get_product( $clave->wcid );
-			$product->set_name( trim(self::xml_attribute($productflds,"desc")) );
-			$clavelinea = self::getClaves("familia",trim(self::xml_attribute($oXml->action[0]->flds[0],"familia")),null);
+			$product->set_name( trim(self::xml_attribute($oFlds,"desc")) );
+			// TODO: Tomar de config que tabla usaran para categorizar
+			$clavelinea = SAIT_UTILS::SAIT_getClaves("familia",trim(self::xml_attribute($oFlds,"familia")),null);
 			if (isset($clavelinea->wcid)) {
 				$product->set_category_ids(array( $clavelinea->wcid));
 			}
 			$product->save();
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("ART UPD");
-			return $res;
+			
+			return SAIT_UTILS::SAIT_response(200,"ART UPD");
 		}
 
 		// Registrar nuevo producto
 		$product = new WC_Product_Simple();
-		$product->set_name( trim(self::xml_attribute($oXml->action[0]->flds[0],"desc")) ); 
-		$product->set_SKU(trim(self::xml_attribute($oXml->action[0]->keys[0],"numart")));
+		$product->set_name( trim(self::xml_attribute($oFlds,"desc")) ); 
+		$product->set_SKU(trim(self::xml_attribute($oKeys,"numart")));
 		$product->set_status("draft");
 		$product->set_manage_stock(true);
-		$clavelinea = self::getClaves("familia",trim(self::xml_attribute($oXml->action[0]->flds[0],"familia")),null);
+		$clavelinea = SAIT_UTILS::SAIT_getClaves("familia",trim(self::xml_attribute($oFlds,"familia")),null);
 		if (isset($clavelinea->wcid)) {
 			$product->set_category_ids(array( $clavelinea->wcid));
 		}
 		$product_id = $product->save();
 		// Guardar en claves
-		self::insertClaves("arts",trim(self::xml_attribute($oXml->action[0]->keys[0],"numart")),$product_id);
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("ART ADD");
-		return $res;
+		SAIT_UTILS::SAIT_insertClaves("arts",trim(self::xml_attribute($oKeys,"numart")),$product_id);
+
+		return SAIT_UTILS::SAIT_response(200,"ART ADD");
 
 	}
 
@@ -138,77 +130,55 @@
 		$SAIT_options=get_option( 'opciones_sait' );
 		$NumAlm = $SAIT_options['SAITNube_NumAlm'];
 		if (isset($NumAlm) && !is_null($NumAlm)) {
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("STOCK ERR ACTEXISGBL");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"STOCK ERR ACTEXISGBL");
 		}
 		foreach ($oXml->action as $action) {
-			$clave = self::getClaves("arts",trim(self::xml_attribute($action->keys[0],"numart")),null);
+			$clave = SAIT_UTILS::SAIT_getClaves("arts",trim(self::xml_attribute($action->keys[0],"numart")),null);
 			if (isset($clave->wcid)) {
 				$product = wc_get_product( $clave->wcid );
 				if ($product===false) {
-					$res = new WP_REST_Response();
-					$res->set_status(200);
-					$res->set_data("ART NO EXISTE");
-					return $res;
+					return SAIT_UTILS::SAIT_response(200,"ART NO EXISTE");
 				}
 				$product->set_stock_quantity(self::xml_attribute($action->flds[0],"existencia"));
 				$product->save();
 			}
 		}
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("STOCK UPD");
-		return $res;
+
+		return SAIT_UTILS::SAIT_response(200,"STOCK UPD");
 	}
 
 	public static function ACTEXIST($oXml){
 		$SAIT_options=get_option( 'opciones_sait' );
 		$NumAlm = $SAIT_options['SAITNube_NumAlm'];
 		if (!isset($NumAlm) && is_null($NumAlm)) {
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("STOCK ERR ACTEXIST Not set");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"STOCK ERR ACTEXIST Not set");
 		}
 		foreach ($oXml->action as $action) {
 			$NumAlmEvent = trim(self::xml_attribute($action->keys[0],"numalm"));
 			if ($NumAlm!=$NumAlmEvent){
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("STOCK ERR ACTEXIST");
-			return $res;
+				return SAIT_UTILS::SAIT_response(200,"STOCK ERR ACTEXIST");
 			}
 
-			$clave = self::getClaves("arts",trim(self::xml_attribute($action->keys[0],"numart")),null);
+			$clave = SAIT_UTILS::SAIT_getClaves("arts",trim(self::xml_attribute($action->keys[0],"numart")),null);
 			if (isset($clave->wcid)) {
 				$product = wc_get_product( $clave->wcid );
 				if ($product===false) {
-					$res = new WP_REST_Response();
-					$res->set_status(200);
-					$res->set_data("ART NO EXISTE");
-					return $res;
+					return SAIT_UTILS::SAIT_response(200,"ART NO EXISTE");
 				}
 				$product->set_stock_quantity(self::xml_attribute($action->flds[0],"existencia"));
 				$product->save();
 			}
 		}
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("STOCK UPD ACTEXIST");
-		return $res;
+		
+		return SAIT_UTILS::SAIT_response(200,"STOCK UPD ACTEXIST");
 	}
 
 	public static function ACTPRECIO($oXml){
 		$numart=trim(self::xml_attribute($oXml->action[0]->keys[0],"numart"));
-		$clave = self::getClaves("arts",$numart,null);
+		$clave = SAIT_UTILS::SAIT_getClaves("arts",$numart,null);
 		$productflds = $oXml->action[0]->flds[0];
 		if (!isset($clave->wcid) or !wc_get_product( $clave->wcid)) {
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("ART NO EXISTE");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"ART NO EXISTE");
 		}
 		$product = wc_get_product( $clave->wcid );
 		if (self::xml_attribute($oXml->action[0]->flds[0],"preciopub")!="") {
@@ -221,20 +191,7 @@
 		$TC = $SAIT_options['SAITNube_TipoCambio'];
 		
 		if ($preciolista != "" || $TC !="" ) {
-			$url = $SAIT_options['SAITNube_URL']."/api/v3/articulos/".$numart;
-			$apikey = $SAIT_options['SAITNube_APIKey'];
-			$args = array(
-				'timeout' => 5,
-				'sslverify' => false,
-				'blocking' => true,
-				'headers' => array(
-					'X-sait-api-key' => $apikey,
-					'Content-Type' => 'application/json',
-					'Accept' => 'application/json',
-				)
-			);
-			$resSAIT =  wp_remote_get($url, $args);
-			$api_response = json_decode(  $resSAIT["body"] , true );
+			$api_response = SAIT_UTILS::SAIT_GetNube("/api/v3/articulos/".$numart);
 			if ($preciolista!=""){
 				$precio = $api_response["result"]["precio".$preciolista];
 				if (floatval($precio)!=0.00){
@@ -250,138 +207,70 @@
 		 		$product->set_regular_price( $precio );
 				$product->save();
 		 	}
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("PRICE UPD API");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"PRICE UPD API");
 				
 		}
 
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("PRICE UPD");
-		return $res;
+		return SAIT_UTILS::SAIT_response(200,"PRICE UPD");
 		
 	}
 
-	public static function MODFAMILIA($oXml){
-		
-	// Agregar catecorias 
-	// https://stackoverflow.com/questions/53460487/add-new-product-categories-programmatically-in-woocommerce
-	// TODO: CEMCO Maneja los Deptos como padres de las familias
-	// el numfamilia tiene 4 digitos siendo los primeros 2 el DPTO
-	//  Se podria usar para agregar el padre automaticamente.
-		$clave = self::getClaves("familia",trim(self::xml_attribute($oXml->action[0]->keys[0],"numfam")),null);
-		if (!isset($clave->wcid)) {
-			$term_data = wp_insert_term(trim(self::xml_attribute($oXml->action[0]->flds[0],"nomfam")), 'product_cat');
-			if( is_wp_error( $term_data ) ) {
-				echo $term_data->get_error_message();
-				$res = new WP_REST_Response();
-				$res->set_status(500);
-				$res->set_data($term_data->get_error_message());
-				return $res;
-			}
-			self::insertClaves("familia",trim(self::xml_attribute($oXml->action[0]->keys[0],"numfam")),$term_data['term_id']);
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("ADD FAM");
-			return $res;
-		}else{
-			$term_data = wp_update_term($clave->wcid,
-				'product_cat',
-				array(
-					'name' => trim(self::xml_attribute($oXml->action[0]->flds[0],"nomfam"))
-				) );
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("UPD FAM");
-				return $res;
-		}
-	}	
-
-	public static function MODDEPTO($oXml){
-		$clave = self::getClaves("deptos",trim(self::xml_attribute($oXml->action[0]->flds[0],"numdep")),null);
+	// MODCATEGORIAWC()
+	//  registra o modifica una categoria en WooCommerce
+	//  $oXml: el evento
+	//  $tabla: nombre de la tabla de la categoria
+	//  $numcat: nombre del campo clave
+	//  $nomcat: campo con el nombre de la categoria
+	public static function MODCATEGORIAWC($oXml,$tabla,$numcat,$nomcat){
+		$clave = SAIT_UTILS::SAIT_getClaves($tabla,trim(self::xml_attribute($oXml->action[0]->keys[0],$numcat)),null);
 		if (!isset($clave->wcid)) {
 			$term_data = wp_insert_term(
-					trim(self::xml_attribute($oXml->action[0]->flds[0],"nomdep")), 
+					trim(self::xml_attribute($oXml->action[0]->flds[0],$nomcat)), 
 					'product_cat'
 			);
 			if( is_wp_error( $term_data ) ) {
-				echo $term_data->get_error_message();
-				$res = new WP_REST_Response();
-				$res->set_status(500);
-				$res->set_data($term_data->get_error_message());
-				return $res;
+				return SAIT_UTILS::SAIT_response(500,$term_data->get_error_message());
 			}
-			self::insertClaves("deptos",trim(self::xml_attribute($oXml->action[0]->flds[0],"numdep")),$term_data['term_id']);
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("ADD DEPTO");
-			return $res;
-		}else{
-			$term_data = wp_update_term($clave->wcid,
-				'product_cat',
-				array(
-					'name' => trim(self::xml_attribute($oXml->action[0]->flds[0],"nomdep"))
-				) );
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("UPD DEPTO");
-				return $res;
-		}
-	}
-
-	public static function MODLINEA($oXml){
-		$clave = self::getClaves("lineas",trim(self::xml_attribute($oXml->action[0]->keys[0],"numlin")),null);
-		if (!isset($clave->wcid)) {
-			$term_data = wp_insert_term(
-					trim(self::xml_attribute($oXml->action[0]->flds[0],"nomlin")), 
-					'product_cat'
-			);
-			if( is_wp_error( $term_data ) ) {
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data($term_data->get_error_message());
-				return $res;
-			}
-			self::insertClaves("lineas",trim(self::xml_attribute($oXml->action[0]->keys[0],"numlin")),$term_data['term_id']);
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("ADD Linea");
-			return $res;
+			SAIT_UTILS::SAIT_insertClaves($tabla,trim(self::xml_attribute($oXml->action[0]->keys[0],$numcat)),$term_data['term_id']);
+			return SAIT_UTILS::SAIT_response(200,"ADD Linea");
 		}else{
 			$term = get_term($clave->wcid);
 			if (is_wp_error($term) ||  is_null($term) ){
 				// cat ya no existe borrarlo de tabla SAIT
 				// para evitar conflictos
-				$clave = self::getClaves("lineas",trim(self::xml_attribute($oXml->action[0]->keys[0]),"numlin"),null);
-				self::deleteClaves($clave->id);
+				$clave = SAIT_UTILS::SAIT_getClaves($tabla,trim(self::xml_attribute($oXml->action[0]->keys[0]),$numcat),null);
+				SAIT_UTILS::SAIT_deleteClaves($clave->id);
 				$term_data = wp_insert_term(
-						trim(self::xml_attribute($oXml->action[0]->flds[0],"nomlin")), 
+						trim(self::xml_attribute($oXml->action[0]->flds[0],$nomcat)), 
 						'product_cat'
 				);
 				if( is_wp_error( $term_data ) ) {
-					$res = new WP_REST_Response();
-					$res->set_status(200);
-					$res->set_data($term_data->get_error_message());
-					return $res;
+					return SAIT_UTILS::SAIT_response(500,$term_data->get_error_message());
 				}
-				self::insertClaves("lineas",trim(self::xml_attribute($oXml->action[0]->keys[0],"numlin")),$term_data['term_id']);
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("ADD Linea");
-				return $res;
+				SAIT_UTILS::SAIT_insertClaves($tabla,trim(self::xml_attribute($oXml->action[0]->keys[0],$numcat)),$term_data['term_id']);
+				return SAIT_UTILS::SAIT_response(200,"ADD ".$tabla);
 			}
 			$term_data = wp_update_term($clave->wcid,
 				'product_cat',
 				array(
-					'name' => trim(self::xml_attribute($oXml->action[0]->flds[0],"nomlin"))
+					'name' => trim(self::xml_attribute($oXml->action[0]->flds[0],$nomcat))
 				) );
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("UPD linea");
-				return $res;
+
+				return SAIT_UTILS::SAIT_response(200,"UPD ".$tabla);
 		}
+
+	}	
+
+	public static function MODFAMILIA($oXml){
+		return self::MODCATEGORIAWC($oXml,"familia","numfam","nomfam");
+	}	
+
+	public static function MODDEPTO($oXml){
+		return self::MODCATEGORIAWC($oXml,"deptos","numdep","nomdep");
+	}
+
+	public static function MODLINEA($oXml){
+		return self::MODCATEGORIAWC($oXml,"lineas","numlin","nomlin");
 	}
 
 	public static function ACTTC($oXml){
@@ -389,31 +278,14 @@
 		$OldTC = $SAIT_options['SAITNube_TipoCambio'];
 		$NewTC=self::xml_attribute($oXml->action[0]->flds[0],"tc");
 		if ($OldTC == $NewTC){
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("Same TC");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"same TC");
 		}
 		$SAIT_options['SAITNube_TipoCambio']=$NewTC;
 		update_option( 'opciones_sait', $SAIT_options );
-		
-		$url = $SAIT_options['SAITNube_URL']."/api/v3/articulos?divisa=D&statusweb=1&limit=10000";
-		$apikey = $SAIT_options['SAITNube_APIKey'];
-		$args = array(
-			'timeout' => 5,
-			'sslverify' => false,
-			'blocking' => true,
-			'headers' => array(
-				'X-sait-api-key' => $apikey,
-				'Content-Type' => 'application/json',
-				'Accept' => 'application/json',
-			)
-		);
-		$resSAIT =  wp_remote_get($url, $args);
-		$api_response = json_decode(  $resSAIT["body"] , true );
-	
+		$api_response = SAIT_UTILS::SAIT_GetNube("/api/v3/articulos?divisa=D&statusweb=1&limit=10000");
+
 		foreach ($api_response["result"] as $row) {
-			$clave = self::getClaves("arts",trim($row["numart"]),null);
+			$clave = SAIT_UTILS::SAIT_getClaves("arts",trim($row["numart"]),null);
 			$product = wc_get_product( $clave->wcid );
 	
 			if ($product===false) {
@@ -423,92 +295,47 @@
 			$product->set_regular_price( $precio );		
 			$product->save();
 		}
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("UPD TC");
-		return $res;
+
+		return SAIT_UTILS::SAIT_response(200,"Upd TC");
 	}
 
 	public static function MODCLI($oXml){
-		// Proceso de MODCLI
 
-		// Saco la clave del articulo
-		$clave = self::getClaves("clientes",trim(self::xml_attribute($oXml->action[0]->keys[0],"numcli")),null);
-
+		// Si no es cliente web omitir
 		$emailtw = trim(self::xml_attribute($oXml->action[0]->flds[0],"emailtw"));
-
 		if ($emailtw==""){
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("no es cliente web");
-			return $res;
-			return;
+			return SAIT_UTILS::SAIT_response(200,"No es cliente web");
 		}
-		// Si ya existe el cliente no hacer nada
-		if (isset($clave->wcid)) {
 
+		// Si ya existe el cliente no hacer nada
+		$clave = SAIT_UTILS::SAIT_getClaves("clientes",trim(self::xml_attribute($oXml->action[0]->keys[0],"numcli")),null);
+		if (isset($clave->wcid)) {
 			// Buscar el cliente si el email es distinto cambiarlo
 			$customer = new WC_Customer( $clave->wcid );
 			$customer->get_email();
 			if ($emailtw != $customer->get_email()){			
 				$customer->set_email( $emailtw );
 				$customer->save();
-				$res = new WP_REST_Response();
-				$res->set_status(200);
-				$res->set_data("cliente actualizado ");
-				return $res;
+				return SAIT_UTILS::SAIT_response(200,"Cliente actualizado");
 			}
 
-			$res = new WP_REST_Response();
-			$res->set_status(200);
-			$res->set_data("cliente ya existe ");
-			return $res;
+			return SAIT_UTILS::SAIT_response(200,"Cliente ya existe");
 		}
-
+		// woocommerce 9.3 requiere estas opciones
+		update_option('woocommerce_registration_generate_password', 'yes');
+		update_option('woocommerce_registration_generate_username', 'yes');
 		// Registrar nuevo cliente
 		$user_id = wc_create_new_customer( $emailtw  );
-		if (!is_int($user_id)) {
-			$res = new WP_REST_Response();
-		 	$res->set_status(200);
-		 	$res->set_data("Ya hay una cuenta registrada con ese correo");
-			return $res;
+		if ( is_wp_error($user_id)) {
+			 return SAIT_UTILS::SAIT_response(200,"Ya hay una cuenta registrada con ese correo");
     	}
 		// Guardar en claves
-		self::insertClaves("clientes",trim(self::xml_attribute($oXml->action[0]->keys[0],"numcli")),$user_id);
-		$res = new WP_REST_Response();
-		$res->set_status(200);
-		$res->set_data("CLI ADD ");
-		return $res;
+		SAIT_UTILS::SAIT_insertClaves("clientes",trim(self::xml_attribute($oXml->action[0]->keys[0],"numcli")),$user_id);
+		
+		return SAIT_UTILS::SAIT_response(200,"Cli ADD");
 
 	}	 
 	 
-	 
-	//
-	// Funciones Claves SAIT
-	// Tabla sait_claves creada en SAIT_WOOCOMMERCE-activator.php
-	//
-	public static function getClaves($tabla,$clave,$wcid){
-		global $wpdb;
-		return $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."sait_claves WHERE tabla = '".$tabla."' and (clave = '".$clave."' or wcid ='" .$wcid."')", OBJECT);
-	}
-	 
-	public static function insertClaves($tabla,$clave,$wcid){
-		global $wpdb;
-		$wpdb->insert( 
-			$wpdb->prefix.'sait_claves', 
-				array( 
-						'tabla' => $tabla,
-						'clave' => $clave,
-						'wcid'  => $wcid
-				)
-		);
-	}
-
-	public static function deleteClaves($id){
-		global $wpdb;
-		$wpdb->delete( $wpdb->prefix.'sait_claves', array( 'id' => $id ) );
-	}
-
 	//
 	// UTILERIAS
 
