1392b771ec08c213f6278fb6118b3eb13f0b5ed7
ali <ali@saitenlinea.com>
2024-11-29 11:45:38 -0700

    add: archivo principal comentado y reorganizado

--- sait-woocommerce/SAIT_WOOCOMMERCE.php
+++ sait-woocommerce/SAIT_WOOCOMMERCE.php
@@ -15,6 +15,9 @@ include plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-options.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_UTILS.php';
 
 
+
+// Variables globales del plugin
+
 if ( !defined( 'SAIT_NUBE_NUMALM' ) ) {
     define( 'SAIT_NUBE_NUMALM', '1' );
 }	
@@ -22,6 +25,21 @@ if ( !defined( 'SAIT_NUBE_NUMALM' ) ) {
 if ( !defined( 'SAIT_SERIE' ) ) {
     define( 'SAIT_SERIE', 'WO' );
 }	
+
+
+// Funciones Inicializacion
+
+function activate_SAIT_WOOCOMMERCE() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-activator.php';
+	SAIT_WOOCOMMERCE_Activator::SAIT_create_db();
+}
+
+
+register_activation_hook( __FILE__, 'activate_SAIT_WOOCOMMERCE' );
+
+
+
+// Router del Plugin
 
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'saitplugin/v1', '/hello',
@@ -32,10 +50,7 @@ add_action( 'rest_api_init', function () {
 	);
 });
 
-function SAIT_helloworld(){
-	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-hello.php';
-	return SAIT_WOOCOMMERCE_Hello::SAIT_helloworld();;
-}
+
 
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'saitplugin/v1', '/saitevents',
@@ -46,6 +61,25 @@ add_action( 'rest_api_init', function () {
 	);
 });
 
+
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'saitplugin/v1', '/testpedido',
+		array(
+			'methods' => 'GET', 
+			'callback' => 'SAIT_testPedido'
+		)
+	);
+});
+
+
+// Callbacks del router
+
+
+function SAIT_helloworld(){
+	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-hello.php';
+	return SAIT_WOOCOMMERCE_Hello::SAIT_helloworld();;
+}
 
 function SAIT_procesEvents($request){
 	$AccessToken = $request->get_header('x-AccessToken');
@@ -70,40 +104,30 @@ function SAIT_procesEvents($request){
 	return SAIT_WOOCOMMERCE_ProcessEvents::SAIT_processEvent($oXml);
 }
 
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'saitplugin/v1', '/testpedido',
-		array(
-			'methods' => 'GET', 
-			'callback' => 'SAIT_testPedido'
-		)
-	);
-});
-
 function SAIT_testPedido(){
 	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-orders.php';
 	return SAIT_WOOCOMMERCE_Orders::SAIT_sendPedidoTest();;
 }
 
-/// Funciones Inicializacion
-
-function activate_SAIT_WOOCOMMERCE() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-activator.php';
-	SAIT_WOOCOMMERCE_Activator::SAIT_create_db();
-}
 
 
-register_activation_hook( __FILE__, 'activate_SAIT_WOOCOMMERCE' );
+// Funciones para procesar eventos disparados por wordpress
+
 
 // Acccion que se ejecuta al hacer pagos con tarjeta o paypal
 add_action( 'woocommerce_payment_complete', 'sendOrderSAIT_payment', 10, 2 );
 // Acccion que se ejecuta al hacer pedidos sin pago
 add_action( 'woocommerce_thankyou', 'sendOrderSAIT_thankyou', 10, 2 );
 
+
+// Order Pagada enviar formapago 1
 function sendOrderSAIT_payment( $order_id ){
 	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-orders.php';
 	SAIT_WOOCOMMERCE_Orders::SAIT_sendOrder($order_id,"1");
 }
 
+
+// Orden sin pago enviar formapago 2
 function sendOrderSAIT_thankyou( $order_id ){
 	require_once plugin_dir_path( __FILE__ ) . 'includes/SAIT_WOOCOMMERCE-orders.php';
 	SAIT_WOOCOMMERCE_Orders::SAIT_sendOrder($order_id,"2");
