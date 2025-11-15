2e43422353c6571f6037892e1b39efb89afe619e
ali <ali@saitenlinea.com>
2025-11-15 10:30:23 -0700

    agregado dryrun y se puso cotizacion a la par

--- sait-woocommerce/includes/SAIT_WOOCOMMERCE-orders.php
+++ sait-woocommerce/includes/SAIT_WOOCOMMERCE-orders.php
@@ -41,6 +41,8 @@
 			$pedido->divisa = "P";
 			$pedido->tc = 1;
 			$pedido->items = [];
+			$pedido->fentrega = date("Ymd"); // 20251113
+			$pedido->hentrega = date("H:i"); // 15:27
 			
 			$Obs_activo = isset($SAIT_options['SAITNube_PedidoObs_Enabled']) && $SAIT_options['SAITNube_PedidoObs_Enabled'] === '1';
 			$Direnvio_activo = isset($SAIT_options['SAITNube_PedidoDirenvio_Enabled']) && $SAIT_options['SAITNube_PedidoDirenvio_Enabled'] === '1';
@@ -62,7 +64,10 @@
 					$art->cant = $item->get_quantity();
 					$product = $item->get_product();
 					$art->numart = $product->get_sku();
-					$art->preciopub = (float)$product->get_regular_price();
+					$api_response = SAIT_UTILS::SAIT_GetNube("/api/v3/articulos/".$art->numart);
+					$art->unidad = $api_response["result"]["unidad"];
+					$art->preciopub =  (float)$product->get_regular_price();
+					$art->precio = (float)$product->get_regular_price();
 					$art->pjedesc1 = self::SAIT_calcularPjeDescuentoItem($art->cant,(float)$item->get_total(),$art->preciopub);
 					$pedido->items[] = $art;
 			}
@@ -93,51 +98,97 @@
 		if (!$FuncionPersonalizadaPedido_activo) {
 			$pedido = SAIT_PERSONALIZADO::SAIT_FuncionPersonalizaPostPedido($pedido,$order);
 		}
-		
-		
-		return SAIT_UTILS::SAIT_PostNube("/api/v3/pedidos",$pedido);
-	}
 
-	public static function SAIT_sendCotizacion( $order ){
+		$api_response =json_decode( wp_remote_retrieve_body( SAIT_UTILS::SAIT_PostNube("/api/v3/pedidos?dryrun=true",$pedido,true)) );
+		if   ($api_response->result=="OK"){
+			// Enviar pedido sin esperar respuesta
+			SAIT_UTILS::SAIT_PostNube("/api/v3/pedidos",$pedido,false);
+		}	
+			return;
+}
+
+public static function SAIT_sendCotizacion( $order,$formapago ){
     // https://wordpress.stackexchange.com/questions/329009/stuck-with-wp-remote-post-sending-data-to-an-external-api-on-user-registration
 
-			$SAIT_options=get_option( 'opciones_sait' );
-			$pedido = new stdClass();
-			$pedido->numdoc = SAIT_SERIE.strval($order->get_id());
+		$SAIT_options=get_option( 'opciones_sait' );
+			$cotizacion = new stdClass();
+			$cotizacion->numdoc = SAIT_SERIE.strval($order->get_id());
 			$date =	$order->get_date_created();
-			$pedido->fecha = $date->date_i18n();
-			$pedido->hora = date('H:i:s',$date->getTimestamp());
-			$pedido->numcli = str_pad("0",5, " ", STR_PAD_LEFT);
-			$pedido->numalm = SAIT_NUBE_NUMALM;
-			// $NumAlm = $SAIT_options['SAITNube_NumAlm'];
-			// if (isset($NumAlm) && !is_null($NumAlm)) {
-			// 	$pedido->numalm = $NumAlm;
-			// }
-			$pedido->formapago = "1";
-			$pedido->divisa = "P";
-			$pedido->tc = 1;
-			$pedido->mostrador = $order->get_formatted_billing_full_name()."\r\n\r\n".$order->get_billing_address_1()."\r\n".$order->get_billing_city().", ".$order->get_billing_state()."\r\n".$order->get_billing_phone();
-			$pedido->items = [];
-			$pedido->obs = self::SAIT_getObs($order);
-			$pedido->direnvio = self::SAIT_getDirEnvio($order);
-			$pedido->otrosdatos = self::SAIT_getOtrosDatos($order);
-			$order_items_data = array_map( function($item){ return $item->get_data(); }, $order->get_items() );
-    		$logger = wc_get_logger();
-    		$logger->add("send-order-debug", json_encode($order_items_data));
-			foreach ( $order->get_items() as $item_id => $item ) {
-					$art = new stdClass();
-					$art->cant = $item->get_quantity();
-					$product = $item->get_product();
-					$art->numart = $product->get_sku();
-					$art->preciopub = (float)$product->get_regular_price();
-					$art->pjedesc1 = self::SAIT_calcularPjeDescuentoItem($art->cant,(float)$item->get_total(),$art->preciopub);
-					$pedido->items[] = $art;
+			$cotizacion->fecha = $date->date_i18n();
+			$cotizacion->hora = date('H:i:s',$date->getTimestamp());
+			$cotizacion->numcli = "";
+			$cotizacion->numcliev = "";
+			$cotizacion->numalm =  str_pad(SAIT_NUBE_NUMALM,2, " ", STR_PAD_LEFT);
+			// Si tiene NumAlm configurado usar ese.
+			$NumAlm = $SAIT_options['SAITNube_NumAlm'];
+			if (isset($NumAlm) && !is_null($NumAlm)) {
+				$cotizacion->numalm =  str_pad($NumAlm,2, " ", STR_PAD_LEFT);
 			}
-			
+			$cotizacion->formapago = $formapago;
+			$cotizacion->divisa = "P";
+			$cotizacion->tc = 1;
+			$cotizacion->items = [];
+			$cotizacion->fentrega = date("Ymd"); // 20251113
+			$cotizacion->hentrega = date("H:i"); // 15:27
+		
+		$Obs_activo = isset($SAIT_options['SAITNube_PedidoObs_Enabled']) && $SAIT_options['SAITNube_PedidoObs_Enabled'] === '1';
+		$Direnvio_activo = isset($SAIT_options['SAITNube_PedidoDirenvio_Enabled']) && $SAIT_options['SAITNube_PedidoDirenvio_Enabled'] === '1';
+		$FuncionPersonalizadaPedido_activo = isset($SAIT_options['SAITNube_FuncionPersonalizadaPedido_Enabled']) && $SAIT_options['SAITNube_FuncionPersonalizadaPedido_Enabled'] === '1';
+		if (!$Obs_activo) {
+			$cotizacion->obs = trim($order->get_customer_note());
+		}
+		if (!$Direnvio_activo) {
+			$cotizacion->direnvio = self::SAIT_getDirEnvio($order);
+		}
 
-			
-			return SAIT_UTILS::SAIT_PostNube("/api/v3/cotizaciones",$pedido);
+
+		foreach ( $order->get_items() as $item_id => $item ) {
+				$art = new stdClass();
+				$art->cant = $item->get_quantity();
+				$product = $item->get_product();
+				$art->numart = $product->get_sku();
+				$art->preciopub =  (float)$product->get_regular_price();
+				$api_response = SAIT_UTILS::SAIT_GetNube("/api/v3/articulos/".$art->numart);
+				$art->unidad = $api_response["result"]["unidad"];
+				$art->precio = (float)$product->get_regular_price();
+				$art->pjedesc1 = self::SAIT_calcularPjeDescuentoItem($art->cant,(float)$item->get_total(),$art->preciopub);
+				$cotizacion->items[] = $art;
+		}
+	$clave = SAIT_UTILS::SAIT_getClaves("clientes",null,$order->get_user_id());		
+	if (isset($clave->clave)){
+		 $cotizacion->numcli =  str_pad($clave->clave,5, " ", STR_PAD_LEFT);
+	}else{
+		// buscar Cliente o Cliente Eventual en SAIT
+		$cotizacion->numcli = SAIT_UTILS::SAIT_getClientebyemail($order->get_billing_email());
+		if ($cotizacion->numcli == ""  ) {
+			$cotizacion->numcliev = SAIT_UTILS::SAIT_getClienteEventualbyemail($order->get_billing_email());
+		}
 	}
+
+	// no se encontro ningun cliente
+	if ($cotizacion->numcli == "" && $cotizacion->numcliev == "" ) {
+			// aqui agregar el objeto clienteventual a cotizacion.
+			$clienteeventual =  new stdClass();
+			$clienteeventual->nomcliev  = $order->get_formatted_billing_full_name();
+			$clienteeventual->calle = $order->get_billing_address_1();
+			$clienteeventual->ciudad = $order->get_billing_city();
+			$clienteeventual->estado = $order->get_billing_state();
+			$clienteeventual->telefono = $order->get_billing_phone();
+			$clienteeventual->email = $order->get_billing_email();
+			$cotizacion->clievent = $clienteeventual;
+	}
+
+	if (!$FuncionPersonalizadaPedido_activo) {
+		$cotizacion = SAIT_PERSONALIZADO::SAIT_FuncionPersonalizaPostPedido($cotizacion,$order);
+	}
+
+	$api_response =json_decode( wp_remote_retrieve_body( SAIT_UTILS::SAIT_PostNube("/api/v3/cotizaciones?dryrun=true",$cotizacion,true)) );
+	if   ($api_response->result=="OK"){
+		// Enviar cotizacion sin esperar respuesta
+		SAIT_UTILS::SAIT_PostNube("/api/v3/cotizaciones",$cotizacion,false);
+	}	
+		return;
+}
 
 
 
@@ -150,14 +201,20 @@
 		if ($tipo==="P"){
 			return self::SAIT_sendPedido($order,$formapago);
 		}else{
-			return self::SAIT_sendCotizacion($order);
+			return self::SAIT_sendCotizacion($order,$formapago);
 		}
 	}
 
 	 
 	public static function SAIT_sendPedidoTest(){
-			$order = wc_get_order( 6151 );
-			return self::SAIT_sendPedido($order,"1");
+			$order = wc_get_order( 0001 );
+			$SAIT_options=get_option( 'opciones_sait' );
+			$tipo = $SAIT_options['SAITNube_TipoDoc'];
+			if ($tipo==="P"){
+				return self::SAIT_sendPedido($order,"1");
+			}else{
+				return self::SAIT_sendCotizacion($order,"1");
+			}
 		}
 
 	public static function SAIT_calcularPjeDescuentoItem($cantidad,$total,$precio){
